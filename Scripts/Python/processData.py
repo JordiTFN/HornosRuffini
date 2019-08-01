dd4247d3b066df534853d25647fb3a81d42edca3
JordiTFN <53252486+JordiTFN@users.noreply.github.com>
2019-08-01 11:27:26 +0200

    Add files via upload

--- Scripts/Python/processData.py
+++ Scripts/Python/processData.py
@@ -1,13 +1,17 @@
 import pandas as pd
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 pathToRawFile = "../../Data/Raw_Data/DatosHornos.txt"
 pathToProcessedFile = "../../Data/Processed_Data/Processed_Data.txt"
 pathToRegistersFile = "../../Data/Processed_Data/Resgisters.txt"
+pathToProcessedRegisters = "../../Data/Processed_Data/Processed_Registers.txt"
 rawDataFile = open(pathToRawFile, 'r')
 processedDataFile = open(pathToProcessedFile, 'w+')
 registersDataFile = open(pathToRegistersFile, 'w+')
 
-registersDataFile.write("[registro];0;id;codigo;nombre;fecha;turno;tipo;cantidad;kgs;;;;;;;;;;;EOL;")
+registersDataFile.write("[registro];0;id;codigo;nombre;fecha;turno;tipo;cantidad;kgs;;;;;;;;;;;EOL;" + '\n')
 dateLines = []
 for line in rawDataFile:
     doWrite = True
@@ -39,6 +43,12 @@ rawDataFile.close()
 processedDataFile.close()
 registersDataFile.close()
 
+#-------------------------------------------------⬆️⬆️⬆️NORMALIZACIÓN INICIAL DE DATOS⬆️⬆️⬆️-------------------------------------------------#
+#==============================================================================================================================================#
+#-------------------------------------------------⬇️⬇️⬇️CREACIÓN DE TABLA DE REGISTROS⬇️⬇️⬇️-------------------------------------------------#
+
+processedRegistersFile = open(pathToProcessedRegisters, 'w+')
+processedRegistersFile.write("fecha;turno;gas;limpieza" + "\n")
 
 registersDataFrame = pd.read_csv(pathToRegistersFile, sep=';', usecols=['id', 'codigo', 'fecha', 'turno', 'tipo', 'cantidad', 'kgs'])
 registersDates = registersDataFrame.fecha.unique()
@@ -46,5 +56,18 @@ registersTurns = registersDataFrame.turno.unique()
 
 for fecha in registersDates:
     for turno in registersTurns:
-        print("---> " + fecha + " / " + str(turno))
-        print(registersDataFrame[registersDataFrame.fecha == fecha][registersDataFrame.turno == turno])
+        sumaGas = registersDataFrame[registersDataFrame.fecha == fecha][registersDataFrame.turno == turno][registersDataFrame.tipo == 'GAS']['cantidad'].sum(axis=0)
+        sumaLimpieza = registersDataFrame[registersDataFrame.fecha == fecha][registersDataFrame.turno == turno][registersDataFrame.tipo == 'LIMPIEZA']['cantidad'].sum(axis=0)
+        processedRegistersFile.write(str(fecha) + ";" + str(turno) + ";" + str(sumaGas) + ";" + str(sumaLimpieza) + "\n")
+
+processedRegistersFile.close()
+os.remove(pathToRegistersFile)
+
+sns.set(style='white')
+testDataFrame = pd.read_csv(pathToProcessedRegisters, sep=';').head(30*3)
+plot = sns.barplot(data=testDataFrame, x='fecha', y='gas', hue='turno', ci=0)
+plot.set(xlabel = "Fecha", ylabel = "Consumo de gas")
+plot.set_xticklabels(labels=plot.get_xticklabels() ,rotation=45)
+#plot.text("Eskere")
+sns.despine()
+plt.show()
