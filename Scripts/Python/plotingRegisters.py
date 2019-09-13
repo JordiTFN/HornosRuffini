067060e51208d974c8b3a7d7ef2b8636b3c3f70d
JordiTFN <53252486+JordiTFN@users.noreply.github.com>
2019-09-13 14:57:13 +0200

    Add files via upload

--- Scripts/Python/plotingRegisters.py
+++ Scripts/Python/plotingRegisters.py
@@ -8,10 +8,12 @@ daysToShow = 30*3
 sns.set(style='white')
 testDataFrame = pd.read_csv(pathToProcessedRegisters, sep=';').head(daysToShow)
 plot1 = sns.barplot(data=testDataFrame, x='fecha', y='gas', hue='turno', ci=0, )
-plot1.set(xlabel = "Fecha", ylabel = "Consumo de gas")
+plot1.set(xlabel = "Fecha", ylabel = "Consuma de Gas")
 plot1.set_xticklabels(labels=plot1.get_xticklabels(), rotation=45)
 
+'''
 plot2 = plot1.twinx()
 plot2.plot(testDataFrame['limpieza'], hue='turno')
+'''
 sns.despine()
 plt.show()
